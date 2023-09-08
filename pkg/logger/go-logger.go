2c17eea40ee8ed2d2cb15dcd7dfca62fffcca4cc
guxueying <xueying.gu@everxyz.onaliyun.com>
2023-09-08 14:50:25 +0800

    [time & info]: finished

--- pkg/logger/go-logger.go
+++ pkg/logger/go-logger.go
@@ -1,6 +1,10 @@
 package logger
 
 import (
+	"fmt"
+	"sync"
+	"time"
+
 	"github.com/natefinch/lumberjack"
 	"go.uber.org/zap"
 	"go.uber.org/zap/zapcore"
@@ -11,100 +15,156 @@ type Logger struct {
 }
 
 type Config struct {
-    InfoLogPath  string
-    ErrorLogPath string
+	InfoLogPath  string
+	ErrorLogPath string
 }
 
-var logInstance *Logger
+var (
+	logInstance *Logger
+	once        sync.Once
+)
 
 func Init(config *Config) {
-    logInstance = NewLogger(config)
+	once.Do(func() {
+		logInstance = NewLogger(config)
+	})
 }
 
 func Info(msg string, tags ...zap.Field) {
-    logInstance.Info(msg, tags...)
+	logInstance.Info(msg, tags...)
 }
 
 func Error(msg string, tags ...zap.Field) {
-    logInstance.Error(msg, tags...)
+	logInstance.Error(msg, tags...)
 }
 
 func Debug(msg string, tags ...zap.Field) {
-    logInstance.Debug(msg, tags...)
+	logInstance.Debug(msg, tags...)
 }
 
 func Warn(msg string, tags ...zap.Field) {
-    logInstance.Warn(msg, tags...)
+	logInstance.Warn(msg, tags...)
 }
 
 func Fatal(msg string, tags ...zap.Field) {
-    logInstance.Fatal(msg, tags...)
+	logInstance.Fatal(msg, tags...)
 }
 
-// ... Add functions for other log levels like Debug, Warn, Fatal ...
+// Formatted logging for Info level
+func Infof(msg string, args ...interface{}) {
+	logInstance.Info(fmt.Sprintf(msg, args...))
+}
+
+// Formatted logging for Error level
+func Errorf(msg string, args ...interface{}) {
+	logInstance.Error(fmt.Sprintf(msg, args...))
+}
+
+// Formatted logging for Debug level
+func Debugf(msg string, args ...interface{}) {
+	logInstance.Debug(fmt.Sprintf(msg, args...))
+}
+
+// Formatted logging for Warn level
+func Warnf(msg string, args ...interface{}) {
+	logInstance.Warn(fmt.Sprintf(msg, args...))
+}
+
+// Formatted logging for Fatal level
+func Fatalf(msg string, args ...interface{}) {
+	logInstance.Fatal(fmt.Sprintf(msg, args...))
+}
+
+func beijingTimeEncoder(t time.Time, enc zapcore.PrimitiveArrayEncoder) {
+	beijingLocation, _ := time.LoadLocation("Asia/Shanghai")
+	enc.AppendString(t.In(beijingLocation).Format(time.RFC3339Nano))
+}
 
 func NewLogger(config *Config) *Logger {
-    // Create a lumberjack logger (from "gopkg.in/natefinch/lumberjack.v2") for file rotation.
-    infoLogWriter := &lumberjack.Logger{
-        Filename:   config.InfoLogPath,
-        MaxSize:    500,  // megabytes after which new file is created
-        MaxBackups: 3,    // number of backups
-        MaxAge:     28,   //days
-    }
+	// Create a lumberjack logger (from "gopkg.in/natefinch/lumberjack.v2") for file rotation.
+	infoLogWriter := &lumberjack.Logger{
+		Filename:   config.InfoLogPath,
+		MaxSize:    500, // megabytes after which new file is created
+		MaxBackups: 3,   // number of backups
+		MaxAge:     28,  //days
+	}
 
-    errorLogWriter := &lumberjack.Logger{
-        Filename:   config.ErrorLogPath,
-        MaxSize:    500,
-        MaxBackups: 3,
-        MaxAge:     28,
-    }
+	errorLogWriter := &lumberjack.Logger{
+		Filename:   config.ErrorLogPath,
+		MaxSize:    500,
+		MaxBackups: 3,
+		MaxAge:     28,
+	}
+	encoderConfig := zap.NewProductionEncoderConfig()
+	encoderConfig.EncodeTime = beijingTimeEncoder
 
-    // Create a zapcore.Core for each log level you need
-    infoCore := zapcore.NewCore(
-        zapcore.NewJSONEncoder(zap.NewProductionEncoderConfig()), 
-        zapcore.AddSync(infoLogWriter),
-        zapcore.InfoLevel,
-    )
+	// Create a zapcore.Core for each log level you need
+	infoCore := zapcore.NewCore(
+		zapcore.NewJSONEncoder(encoderConfig),
+		zapcore.AddSync(infoLogWriter),
+		zapcore.InfoLevel,
+	)
 
-    errorCore := zapcore.NewCore(
-        zapcore.NewJSONEncoder(zap.NewProductionEncoderConfig()), 
-        zapcore.AddSync(errorLogWriter),
-        zapcore.ErrorLevel,
-    )
+	errorCore := zapcore.NewCore(
+		zapcore.NewJSONEncoder(encoderConfig),
+		zapcore.AddSync(errorLogWriter),
+		zapcore.ErrorLevel,
+	)
 
-    // Combine them together
-    core := zapcore.NewTee(infoCore, errorCore)
+	// Combine them together
+	core := zapcore.NewTee(infoCore, errorCore)
 
-    // Create a zap logger with the combined core
-    zlog := zap.New(core)
+	// Create a zap logger with the combined core
+	zlog := zap.New(core)
 
-    return &Logger{zap: zlog}
+	return &Logger{zap: zlog}
 }
 
-
 func (l *Logger) Info(msg string, tags ...zap.Field) {
-    l.zap.Info(msg, tags...)
+	l.zap.Info(msg, tags...)
 }
 
 func (l *Logger) Error(msg string, tags ...zap.Field) {
-    l.zap.Error(msg, tags...)
+	l.zap.Error(msg, tags...)
 }
 
 func (l *Logger) Debug(msg string, tags ...zap.Field) {
-    l.zap.Debug(msg, tags...)
+	l.zap.Debug(msg, tags...)
 }
 
 func (l *Logger) Warn(msg string, tags ...zap.Field) {
-    l.zap.Warn(msg, tags...)
+	l.zap.Warn(msg, tags...)
 }
 
 func (l *Logger) Fatal(msg string, tags ...zap.Field) {
-    l.zap.Fatal(msg, tags...)
+	l.zap.Fatal(msg, tags...)
+}
+
+// Formatted logger methods
+
+func (l *Logger) Infof(msg string, args ...interface{}) {
+	l.zap.Info(fmt.Sprintf(msg, args...))
+}
+
+func (l *Logger) Errorf(msg string, args ...interface{}) {
+	l.zap.Error(fmt.Sprintf(msg, args...))
+}
+
+func (l *Logger) Debugf(msg string, args ...interface{}) {
+	l.zap.Debug(fmt.Sprintf(msg, args...))
+}
+
+func (l *Logger) Warnf(msg string, args ...interface{}) {
+	l.zap.Warn(fmt.Sprintf(msg, args...))
+}
+
+func (l *Logger) Fatalf(msg string, args ...interface{}) {
+	l.zap.Fatal(fmt.Sprintf(msg, args...))
 }
 
 // ... Implement similar functions for other log levels like Debug, Warn, Fatal ...
 
 // Cleanup should be called to ensure all log messages are flushed
 func Cleanup() {
-    logInstance.zap.Sync()
+	logInstance.zap.Sync()
 }
